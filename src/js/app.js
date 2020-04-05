9fddefb198866542e3df00ebc9704df19b1cf497
Ryszard Glegola <ryszard.glegola@gmail.com>
2020-04-05 18:31:19 +0200

    Add working page links and support for URL hash errors

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,75 @@
-import {settings, select} from './settings.js'; // ./ essential, points to a separate file
+import {settings, select, classNames} from './settings.js'; // ./ essential, points to a separate file
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
 
 const app = {
+  initPages: function(){
+    const thisApp = this;
+
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+    const idFromHash = window.location.hash.replace('#/', '');
+    
+    let pageMatchingHash = thisApp.pages[0].id;
+
+    for (let page of thisApp.pages){
+      if(page.id == idFromHash){
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+  
+    thisApp.activatePage(pageMatchingHash);
+    window.location.hash = '#/' + pageMatchingHash;
+
+    // Add listeners to nav links 
+    for (let link of thisApp.navLinks){
+      link.addEventListener('click',function(event){
+        const clickedElement = this;
+        event.preventDefault();
+
+        /* get page id from href attribute and get rid of hash sign from it */
+        const id = clickedElement.getAttribute('href').replace('#', '');
+
+        /* activate the page with that id, i.e. run thisApp.activatePage(pageId) */
+        thisApp.activatePage(id);
+
+        /* change URL hash */
+        window.location.hash = '#/' + id;
+
+      });
+    }
+  },
+
+  activatePage: function(pageId){
+    const thisApp = this;
+
+    /* add class "active" to matching page, remove from non-matching */
+    for (let page of thisApp.pages){
+      // instead of this...
+      /* if (page.id == pageId){
+        page.classList.add(classNames.pages.active);
+      } else {
+        page.classList.remove(classNames.pages.active);
+      }
+      */
+      // let's use toggle like this:
+      page.classList.toggle(
+        classNames.pages.active, 
+        page.id == pageId
+      );     
+    }
+
+    /* add class "active" to matching nav link, remove from non-matching */
+    for (let link of thisApp.navLinks){
+      link.classList.toggle(
+        classNames.nav.active, 
+        link.getAttribute('href') == '#' + pageId
+      );     
+    }
+    
+  },
+
   initMenu: function(){
     const thisApp = this;
 
@@ -25,8 +92,7 @@ const app = {
         return rawResponse.json();
       })
       .then(function(parsedResponse){
-        console.log('"ParsedResponse" received in app.InitData:',parsedResponse);
-
+        
         /* save parsedResponse as thisApp.data.products */
         thisApp.data.products = parsedResponse;
 
@@ -63,6 +129,7 @@ const app = {
     thisApp.initData();
     // thisApp.initMenu(); /* this was moved inside thisApp.initData */
     thisApp.initCart();
+    thisApp.initPages();
   },
 };
 
